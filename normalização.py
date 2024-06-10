f783281bb3187e83821c2db858bdb8ea8aa44135
anafrnandes <ana.almeida.fernandes@ubi.pt>
2024-06-10 18:50:18 +0100

    Criaçao da normalizaçao do sem outliers

---
+++ normalização.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+# Load the data
+data = pd.read_csv('merged_sem_outliers.csv')
+
+numerical_features = data.select_dtypes(include=[np.number]).columns.tolist()
+
+print("\nColunas Numéricas Selecionadas para Normalização:")
+print(numerical_features)
+
+scaler = MinMaxScaler()
+data[numerical_features] = scaler.fit_transform(data[numerical_features])
+
+print("\nDados Normalizados:")
+print(data)
